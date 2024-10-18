0d1a0fa76854f5f28ecf070a8f57a0b691000bf1
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-19 01:21:03 +0700

    Updated

---
+++ src/js/swiper.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.swiper-books', {
+    direction: "horizontal",
+    slidesPerView: 'auto',
+    spaceBetween: 14,
+    slidesOffsetBefore: 24,
+    slidesOffsetAfter: 24,
+  });
